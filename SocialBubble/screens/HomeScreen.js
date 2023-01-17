2649dbe6f0d22829ca6a5cde5eea014a996a6a03
Calum Giffin <2407426g@student.gla.ac.uk>
2023-01-17 22:01:23 +0000

    Update HomeScreen.js to work in accordance with nav bar added to app.js

--- SocialBubble/screens/HomeScreen.js
+++ SocialBubble/screens/HomeScreen.js
@@ -1,40 +1,18 @@
-import {React, useState} from 'react';
+import {React} from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Dimensions} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import Login from './LoginScreen'
-import Home from './HomeScreen'
-import Settings from './SettingsScreen'
-import BubbleChat from './ChatScreen'
-
-const Tab = createBottomTabNavigator();
 
 export default function HomeScreen(props) {
         return (
-            <View style = {styles.container}>
             <View style = {styles.title}>
             <Text style = {styles.titleText}>SocialBubbleHome</Text>
-            </View>
-
-            <NavigationContainer>
-                <Tab.Navigator>
-                    <Tab.Screen name ="Home" component={Home}/>
-                    <Tab.Screen name ="Login" component={Login}/>
-                    <Tab.Screen name ="Friends"/>
-                    <Tab.Screen name ="Settings" component={Settings}/>
-                    <Tab.Screen name ="OuterBubble" component={BubbleChat}/>
-                </Tab.Navigator>
-            </NavigationContainer>
-
-            </View>
+            <StatusBar style='auto'></StatusBar>
+            </View>    
         );
 
       }
-      
 
 const windowWidth = Dimensions.get('window').width;
-
 const styles = StyleSheet.create({
     container:{
         flex:1,
