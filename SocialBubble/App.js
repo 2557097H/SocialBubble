22d485a3cc88dc53828793af4e926edcf5e8e5ed
Calum Giffin <2407426g@student.gla.ac.uk>
2023-01-17 22:00:27 +0000

    Updated app.js with required nav bar.

--- SocialBubble/App.js
+++ SocialBubble/App.js
@@ -1,14 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-//imports LoginScreen from the screens folder
+import { NavigationContainer } from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import LoginScreen from './screens/LoginScreen';
-import ChatScreen from './screens/ChatScreen'
+import ChatScreen from './screens/ChatScreen';
+import HomeScreen from './screens/HomeScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
+const Tab = createBottomTabNavigator();
 export default function App() {
-  // when the app is open simply calls the login screen
-  // later this can be changed to display the home page if the user is loged in
-  // else the login screen
   return(
-    <ChatScreen/>
-  );
+    <NavigationContainer>
+    <Tab.Navigator>
+            <Tab.Screen name ="Home" component={HomeScreen}/>
+            <Tab.Screen name ="Login" component={LoginScreen}/>
+            <Tab.Screen name ="Friends" component={HomeScreen}/>
+            <Tab.Screen name ="Settings" component={SettingsScreen}/>
+            <Tab.Screen name ="OuterBubble" component={ChatScreen}/>
+        </Tab.Navigator>
+    </NavigationContainer>  
+);
 }
+
