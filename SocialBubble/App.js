9e731c2cd985b796dd7de136240363078e5463d2
Calum Giffin <2407426g@student.gla.ac.uk>
2023-01-18 00:48:10 +0000

    Removed stack navigator and added elements for details and preferences to bottom nav bar for showcase to client tomorrow. If stack navigation is wanted as well we can work on embedding navigation containers after 3rd client meeting.

--- SocialBubble/App.js
+++ SocialBubble/App.js
@@ -4,43 +4,24 @@ import LoginScreen from './screens/LoginScreen';
 import ChatScreen from './screens/ChatScreen';
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import PersonalDetailsScreen from './screens/PersonalDetailsScreen';
 import PreferencesScreen from './screens/PreferencesScreen';
 
-const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return(
-    <NavigationContainer>
 
+    //NavigationBar elements, WIll be updated with default routes and icons in later commits
+    <NavigationContainer>
     <Tab.Navigator>
             <Tab.Screen name ="Home" component={HomeScreen}/>
             <Tab.Screen name ="Login" component={LoginScreen}/>
-            <Tab.Screen name ="Friends" component={HomeScreen}/>
-            <Tab.Screen name ="Settings" component={SettingsScreen}/>
             <Tab.Screen name ="OuterBubble" component={ChatScreen}/>
+            <Tab.Screen name ="Settings" component={SettingsScreen}/>
+            <Tab.Screen name ="Preferences" component={PreferencesScreen}/>
+            <Tab.Screen name ="Details" component={PersonalDetailsScreen}/>          
         </Tab.Navigator>
-
-    <Stack.Navigator>
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="Login"
-          component={LoginScreen}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="Details"
-          component={PersonalDetailsScreen}
-        />
-        <Stack.Screen
-          options={{headerShown: false}}
-          name="Preferences"
-          component={PreferencesScreen}
-        />
-    </Stack.Navigator>
-
     </NavigationContainer>  
 );
 }
