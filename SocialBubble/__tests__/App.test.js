f6fd0dddd572a7116885d2792f3bb9a6a515d7b7
Julian Lee <2600433l@student.gla.ac.uk>
2023-02-01 18:37:28 +0000

    Update SocialBubble/__tests__/App.test.js

--- SocialBubble/__tests__/App.test.js
+++ SocialBubble/__tests__/App.test.js
@@ -4,3 +4,10 @@ import renderer from 'react-test-renderer';
 import App from '../App';
 
 //https://docs.expo.dev/guides/testing-with-jest/
+
+describe('<App />', () => {
+    it('has 2 child', () => {
+        const tree = renderer.create(<App />).toJSON();
+        expect(tree.children.length).toBe(2);
+      });
+});
