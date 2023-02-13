a120bc416c74dc8ce491bd1ec841ca95744a9df1
Calum Giffin <calumgiff1997@gmail.com>
2023-02-13 14:19:37 +0000

    FirebaseSetUpandRealTimeDatabaseConfigComplete

--- SocialBubble/App.js
+++ SocialBubble/App.js
@@ -11,8 +11,30 @@ import PasswordChangedScreen from './screens/PasswordChangedScreen';
 import ConfirmPasswordScreen from './screens/ConfirmPasswordScreen';
 import DeleteAccountScreen from './screens/DeleteAccountScreen';
 import AccountDeletedScreen from './screens/AccountDeletedScreen';
-const Tab = createBottomTabNavigator();
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCQGbg_k9aE5FxsPHB9FenzAONu4_yNfWE",
+  authDomain: "socialbubble-ff070.firebaseapp.com",
+  projectId: "socialbubble-ff070",
+  storageBucket: "socialbubble-ff070.appspot.com",
+  messagingSenderId: "426846827737",
+  appId: "1:426846827737:web:58cdaded0ed89d490afac8",
+  measurementId: "G-1EQTFQNESR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const Tab = createBottomTabNavigator();
 export default function App() {
   return(
 
